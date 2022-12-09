c6cc68e265a462f72c1002f2f4f54e07631a86dc
cpanato <ctadeu@gmail.com>
2022-12-09 15:19:58 +0100

    use underscore in the resource name

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "uptime-check" {
-  value = local.use_gclb ? google_monitoring_uptime_check_config.global-uptime-check[0].uptime_check_id : google_monitoring_uptime_check_config.regional-uptime-check[0].uptime_check_id
+output "uptime_check" {
+  value = local.use_gclb ? google_monitoring_uptime_check_config.global_uptime_check[0].uptime_check_id : google_monitoring_uptime_check_config.regional_uptime_check[0].uptime_check_id
+}
+
+output "uptime_check_name" {
+  value = local.use_gclb ? google_monitoring_uptime_check_config.global_uptime_check[0].display_name : google_monitoring_uptime_check_config.regional_uptime_check[0].display_name
 }
